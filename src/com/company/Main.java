b7a3d0b04ebdc6a832b7f6bd66f5bb62b10814b1
Brian B <brianb@MacBook-Pro-4.local>
2019-01-17 21:40:27 -0500

    Adding system out and in for stage two

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,24 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-    Scanner userInput = new Scanner();
+    Scanner userInput = new Scanner(System.in);
 
 
     String lineBreak = "\n";
     String initialPrompt = "You're in a stank bog and all the peet and crud-water is catching fire around you. Do you: ";
 
     String firstStageOptionOne = "1 - Float away by grabbing a lifting hot air balloon";
-    String firstStageOptionTwo = "2 - Hop on the back of a passing american-bad-ass eagle by gymnastic-ing up the beasts talons";
+    String firstStageOptionTwo = "2 - Hop on the back of a passing american-bad-ass eagle by kungfuing up its talons";
 
 
     String stageTwoBalloonPrompt = "You are now 100 ft in the air. Do you:";
-    String stageTwoBalloonOptionOne = "Burn the mother down and stoke the balloon flame to gain lift";
-    String stageTwoBalloonOptionTwo = "Just give up your will to live and take a nap in the balloon basket";
+    String stageTwoBalloonOptionOne = "1 - Burn the mother down and stoke the balloon flame to gain lift";
+    String stageTwoBalloonOptionTwo = "2 - Just give up your will to live and take a nap in the balloon basket";
 
     String stageTwoEaglePrompt = "You are now riding the spine of a bucking american-bad-ass eagle. Do you:";
-    String stageTwoEagleOptionOne = "Sing Oh Canada and feed it maple syrup";
-    String stageTwoEagleOptionTwo = "Sing The Star Spangled Banner and feed it over-priced healthcare";
-    
+    String stageTwoEagleOptionOne = "1 - Sing Oh Canada and feed it maple syrup";
+    String stageTwoEagleOptionTwo = "2- Sing The Star Spangled Banner and feed it over-priced healthcare";
+
     boolean wantToContinue = true;
 
     do{
@@ -31,15 +31,19 @@ public class Main {
         int userAnswer = userInput.nextInt();
 
         if(userAnswer == 1){
-            System.out.println();
+            System.out.println(stageTwoBalloonPrompt + lineBreak + stageTwoBalloonOptionOne + lineBreak + stageTwoBalloonOptionTwo);
+            userAnswer = userInput.nextInt();
         } else if (userAnswer == 2){
 
-
+            System.out.println(stageTwoEaglePrompt + lineBreak + stageTwoEagleOptionOne + lineBreak + stageTwoEagleOptionTwo);
+            userAnswer = userInput.nextInt();
 
 
 
 
         }
+
+        
 
     }while(wantToContinue);
 
